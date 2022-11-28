a449c68a8c130d38097e3e0baa9402a0531c4340
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-11-27 19:17:50 -0600

    working in the routes index.js file

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { response } = require('express');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -6,6 +7,13 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  console.log("getting categories", req, res)
+  Category.findAll().then((categories) => {
+    if (!categories) res.status(400).json({ "message": "cannot find categories" })
+    console.log(categories)
+    res.status(200).json(categories)
+  })
+    .catch((err) => res.status(500).json({ "message": "internal server error" }))
 });
 
 router.get('/:id', (req, res) => {
